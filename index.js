5a4628815746764ee02610fd8441890d29ab3871
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-02 02:32:02 -0400

    aun con problemas con lodash y chalk

--- index.js
+++ index.js
@@ -2,34 +2,52 @@ const express = require('express');
 const axios = require("axios");
 const { v4: uuidv4 } = require('uuid');
 const moment = require('moment');
-const _ = require('lodash');
+//const chalk = require('chalk');
+const _ = require("lodash/partition");
+/* const { partition } =  require('lodash/partition'); */
 const app = express(); 
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3000;
 const url = "https://randomuser.me/api";
 const persona = [];
 moment.locale('es');
 
-
-
-app.use('/usuarios',(req, res) => {
+app.use('/usuarios',async (req, res) => {
   //funcion que captura datos desde random user api, con axios
-  axios.get(url).then((data) => {
-    const usuario = data.data;
-    const personaRandom = {
-      id: uuidv4().slice(0, 8), // se asigna id usando uuid v4, considerando los primeros 8 caracteres
-      gender: usuario.results[0].gender,
-      first: usuario.results[0].name.first,
-      last: usuario.results[0].name.last,
-      timeStamp: moment().format("MMMM Do YYYY, h:mm:ss a"),
-    }; // se agrega la fecha usando moment y formateado para ser guardada en el array
-    persona.push(personaRandom); //guarda la persona en el array
-  });
-    console.log( filtered_array = _.filter(persona, ["gender", "male"]));
+    axios.get(url).then((data) => {
+        const usuario = data.data;
+        const personaRandom = {
+            id: uuidv4().slice(0, 8), // se asigna id usando uuid v4, considerando los primeros 8 caracteres
+            gender: usuario.results[0].gender,
+            first: usuario.results[0].name.first,
+            last: usuario.results[0].name.last,
+            timeStamp: moment().format("MMMM Do YYYY, h:mm:ss a"),
+        }; // se agrega la fecha usando moment y formateado para ser guardada en el array
+        persona.push(personaRandom); //guarda la persona en el array
+    });
+
+    const mujeres = _.partition(persona, (user) => {
+        user.gender == "female"
+    });
+    console.log(mujeres[0]);
+    console.log(mujeres[1]);
+    /* const despliegue = `<h3>Mujeres</h3>
+                    <ol> ${mujeres[0].map((user) => {
+                        return `<li>Nombre: ${user.first} - Apellido: ${user.last} - Id: ${user.id} - Hora: ${user.timeStamp}</li>`;
+                    })}
+                    </ol>
+                        <h3>Hombres</h3>
+                    <ol> ${mujeres[1].map((user) => {
+                        return `<li>Nombre: ${user.first} - Apellido: ${user.last} - Id: ${user.id} - Hora: ${user.timeStamp}</li>`;
+                    })}
+                    </ol>   `; */
     
-});
+   /*  console.log( chalk.blue.bgWhite(
+        `Nombre: ${user.first} - Apellido: ${user.last} - Id: ${user.id} - Hora: ${user.timeStamp} `
+      ));  */
+   
+    res.send(despliegue);
+  });
 
-
-
-app.listen(port, () => {
+app.listen( port, () => {    
     console.log(`Server running on port http://localhost:${port}`);  
 })
